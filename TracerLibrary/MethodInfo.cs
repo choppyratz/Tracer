326c42bbf1da5774ee7808010e3a0b355cc64582
German <GermanletchiK@yandex.ru>
2020-09-09 13:00:20 +0300

    add method and thread structure for tree building

---
+++ TracerLibrary/MethodInfo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using System.Diagnostics;
+
+namespace TracerLibrary
+{
+    [Serializable]
+    public class MethodInfo
+    {
+        [JsonProperty("class")]
+        public string _className;
+
+        [JsonProperty("name")]
+        public string _methodName;
+
+        [JsonProperty("time")]
+        public int millisecinds;
+
+        [JsonIgnore]
+        public MethodInfo parent = null;
+
+        [JsonIgnore]
+        public Stopwatch _tracer;
+
+        public List<MethodInfo> methods;
+
+        public MethodInfo(string className, string methodName, Stopwatch tracer)
+        {
+            methods = new List<MethodInfo> { };
+            _className = className;
+            _methodName = methodName;
+            _tracer = tracer;
+        }
+
+        public MethodInfo()
+        {
+
+        }
+    }
+}
---
+++ TracerLibrary/ThreadInfo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using System.Diagnostics;
+
+namespace TracerLibrary
+{
+    [Serializable]
+    public class ThreadInfo
+    {
+
+        [JsonProperty("id")]
+        public int _id;
+
+        [JsonProperty("time")]
+        public int millisecinds;
+
+        [JsonIgnore]
+        public MethodInfo currMethod = null;
+
+        [JsonIgnore]
+        public MethodInfo parent = null;
+
+        [JsonIgnore]
+        public Stopwatch _tracer;
+
+
+        [JsonProperty("methods")]
+        public List<MethodInfo> stackContext = new List<MethodInfo> { };
+
+        public ThreadInfo(int id, Stopwatch tracer)
+        {
+            _id = id;
+            _tracer = tracer;
+        }
+
+        public ThreadInfo()
+        {
+
+        }
+    }
+}
